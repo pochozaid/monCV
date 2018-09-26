599abb62d1c3045e98477ea658bf6222b9309ad7
Zaid Bennani <zaidbennani22@gmail.com>
2018-09-26 14:53:19 +0200

    js

--- src/javascripts.js
+++ src/javascripts.js
@@ -17,3 +17,26 @@ function affiche(id)
   }
   return true;
 }
+function Progressive(id) {
+  var divid=document.getElementById(id);
+  var list = document.getElementsByClassName("p2");
+  [].forEach.call(list, function (i) {i.classList.remove("p2");
+  if(i!==divid)
+  i.style.display="none";
+  });
+  if(divid.style.display == 'none'){
+  divid.classList.add("p2");
+  divid.style.display='inline';
+  }
+  else{
+  divid.style.display='none';
+  } 
+  var myAwesomeVar = 2;
+  //let timerId = setInterval(function (){var liste = document.getElementsByClassName("p2");liste[0].style.fontSize= myAwesomeVar++ + "px";}, 50);
+
+  //let timerId = setInterval(function (){[].forEach.call(list, function (i) {i.style.fontSize= myAwesomeVar + "px";myAwesomeVar++;})}, 150);
+
+  let timerId = setInterval(function (){var i=0;list[i].style.fontSize= myAwesomeVar++ + "px";i++;}, 70);
+  // after 2 seconds stop
+  setTimeout(() => { clearInterval(timerId);}, 1000);
+}
